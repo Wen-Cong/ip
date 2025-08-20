a770aca0329dfa033139c2cedcd6128f56d81f13
Wen-Cong <wencong.dev@gmail.com>
2025-08-20 12:04:45 +0800

    Add function to read and echo input command given by the user and exit program with command "bye"
    Add echo and promptUser function under Bot class to allow a Bot to perform these operations

--- src/main/java/Bot.java
+++ src/main/java/Bot.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Bot {
     /** Name of the bot */
     private final String name;
@@ -17,10 +19,34 @@ public class Bot {
     }
 
     /**
-     * Print program exit message
+     * Print program exit message and exit the program
      */
     public void exit() {
         System.out.println("Bye. Hope to see you again soon!");
+        this.printSeparator();
+        System.exit(0);
+    }
+
+    /**
+     * Prompt the user for input
+     *
+     * @param scanner Scanner object to read input from console
+     * @return Command string input entered by the user
+     */
+    public String promptUser(Scanner scanner) {
+        System.out.print("Enter your command: ");
+        String cmd = scanner.nextLine();
+        this.printSeparator();
+        return cmd;
+    }
+
+    /**
+     * Print the message of the given String
+     *
+     * @param msg Message to be echoed and printed
+     */
+    public void echo(String msg) {
+        System.out.println(msg);
         this.printSeparator();
     }
 
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,7 +1,27 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
+        // Init a Bot to begin program
         Bot bot = new Bot("Lovely");
+        // Create a Scanner object to read input from the console
+        Scanner scanner = new Scanner(System.in);
+
+        // Print greeting message
         bot.greet();
-        bot.exit();
+
+        while (true) {
+            // Read input from console
+            String command = bot.promptUser(scanner);
+
+            // Redirect command to invoke operations
+            switch (command) {
+            case "bye":
+                bot.exit();
+            default:
+                bot.echo(command);
+            }
+
+        }
     }
 }
