da93716ba81c4e944deb10ceac78fa0805c244f1
Wen-Cong <wencong.dev@gmail.com>
2025-08-21 22:12:40 +0800

    Add new Exception class InvalidCommandException to handle input commands with invalid format
    This commit introduce a new exception InvalidCommandException that is to be thrown and managed when a command format input by the user is invalid.
    
    This can help to identify specific exception for incorrect command format and program can use this exception to ensure the program is robust even when there is a typo in user input

---
+++ src/main/java/InvalidCommandException.java
@@ -0,0 +1,10 @@
+public class InvalidCommandException extends Exception {
+    /**
+     * Constructor
+     *
+     * @param message Message regarding more info of Exception thrown
+     */
+    public InvalidCommandException(String message) {
+        super(message);
+    }
+}
